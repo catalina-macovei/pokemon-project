54ca2ba53a08522e8cb8e4ed2ecf19f27d1c965d
catalina-macovei <cmacovei01@gmail.com>
2022-10-03 16:06:00 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+const container=document.querySelector("#container");
+const baseURL='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+for(let i=0; i<100; i++){
+    const newImg= document.createElement('img');
+    newImg.src=`${baseURL}${i}.png`;
+    container.appendChild(newImg);
+    
+}
